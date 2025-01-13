83267873db1e154e8e5eadc3f13442502e10455e
donggyu <use02012@gmail.com>
2025-01-13 16:37:04 +0900

    init: 프로젝트 생성
    - 기본 스프링부트 프로젝트 구조

---
+++ src/main/java/com/ssafy/solvedpick/SolvedPickApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.solvedpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvedPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvedPickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/solvedpick/SolvedPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.solvedpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvedPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
