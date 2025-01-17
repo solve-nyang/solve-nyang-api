a2d527f713d2020cfa41b100f9409c6e4b3f859e
donggyu <use02012@gmail.com>
2025-01-17 13:56:28 +0900

    feat: 내 정보 조회 API 구현
    - 외부 API로부터 받아온 값을 조작하여 원하는 형태로 반환

---
+++ src/main/java/com/ssafy/solvedpick/members/dto/UserInfoResponse.java
@@ -0,0 +1,15 @@
+package com.ssafy.solvedpick.members.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInfoResponse {
+    private String nickname;
+    private int point;
+    private int solvedacTier;
+    private int solvedCount;
+    private int solvedacStrick;
+}
