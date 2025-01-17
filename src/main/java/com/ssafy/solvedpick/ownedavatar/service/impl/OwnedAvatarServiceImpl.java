8202dda67e6eba031150ff823c1bd9a6273af4a6
donggyu <use02012@gmail.com>
2025-01-17 15:42:25 +0900

    feat: 사용자 획득 캐릭터 상태 변경 기능 추가
    - 사용자가 획득한 캐릭터의 선택 여부에 따라 현재 visible 상태를 변화

--- src/main/java/com/ssafy/solvedpick/ownedavatar/service/impl/OwnedAvatarServiceImpl.java
+++ src/main/java/com/ssafy/solvedpick/ownedavatar/service/impl/OwnedAvatarServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ssafy.solvedpick.ownedavatar.service.impl;
 
 import com.ssafy.solvedpick.avatars.domain.Grade;
+import com.ssafy.solvedpick.ownedavatar.domain.OwnedAvatar;
 import com.ssafy.solvedpick.ownedavatar.dto.OwnedAvatarDTO;
 import com.ssafy.solvedpick.ownedavatar.repository.OwnedAvatarRepository;
 import com.ssafy.solvedpick.ownedavatar.service.OwnedAvatarService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +31,17 @@ public class OwnedAvatarServiceImpl implements OwnedAvatarService {
                         .dropRate(Grade.fromValue(ownedAvatar.getAvatar().getGrade()).getProbability())
                         .build())
                 .toList();
+    }
+
+//    Todo: member정보 확인
+    @Override
+    public void updateAvatarVisibility(Long memberId, Long avatarId) {
+        OwnedAvatar ownedAvatar = ownedAvatarRepository.findById(avatarId)
+                .orElseThrow(() -> new EntityNotFoundException("No avatar"));
+
+        if (!ownedAvatar.getMember().getId().equals(memberId)) {
+            throw new IllegalArgumentException("Not authorized to update this avatar");
+        }
+        ownedAvatar.updateVisibility();
     }
 }
