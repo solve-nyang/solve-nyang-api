4573498575b77a35437d598f924a2e7df72b55c1
JIYOUNG KIM <kandallee38@gmail.com>
2025-01-20 23:12:27 +0900

    refactor: Refactor code
    Replace map to instance

--- src/main/java/com/ssafy/solvedpick/accounts/presentation/AccountController.java
+++ src/main/java/com/ssafy/solvedpick/accounts/presentation/AccountController.java
@@ -1,24 +1,17 @@
 package com.ssafy.solvedpick.accounts.presentation;
 
+import com.ssafy.solvedpick.accounts.dto.*;
+import com.ssafy.solvedpick.common.dto.ResponseMessageDTO;
+import com.ssafy.solvedpick.global.error.dto.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 
-import java.time.Duration;
-import java.util.Map;
-
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.solvedpick.accounts.dto.SignupFormDTO;
-import com.ssafy.solvedpick.accounts.dto.SignInFormDTO;
-import com.ssafy.solvedpick.accounts.dto.TokenResponse;
-import com.ssafy.solvedpick.accounts.dto.MembernameDTO;
 import com.ssafy.solvedpick.accounts.service.SignupService;
 import com.ssafy.solvedpick.accounts.service.SigninService;
 import com.ssafy.solvedpick.accounts.service.VerificationService;
@@ -34,51 +27,44 @@ public class AccountController {
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupFormDTO signupFormDTO) {
     	boolean verified = verificationService.verifyUser(signupFormDTO);
+
     	if (verified) {
     		signupService.create(signupFormDTO);
-    		return ResponseEntity.ok().body(Map.of("message", "success"));
+    		return ResponseEntity.ok()
+                    .body(ResponseMessageDTO.builder()
+                            .message("success")
+                            .build());
     	}
-    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", "failed"));
+    	return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseMessageDTO.builder()
+                        .message("failed")
+                        .build());
     }
     
     
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody SignInFormDTO signInFormDTO) {
         TokenResponse tokenResponse = signinService.signIn(signInFormDTO);
-        final String accessToken = tokenResponse.getAccessToken();
-//        final String refreshToken = tokenResponse.getRefreshToken();
-        
-//        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", accessToken)
-//        	       .httpOnly(true)
-//        	       .secure(true)  // HTTPS 사용시.
-//        	       .sameSite("None") // 추후 포트 맞출 필요 있음
-//        	       .maxAge(Duration.ofDays(1))
-//        	       .path("/")
-//        	       .build();
-        	       
-//        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
-//        	       .httpOnly(true)
-//        	       .secure(true)  // HTTPS 사용시.
-//        	       .sameSite("None")  // 추후 포트 맞출 필요 있음
-//        	       .maxAge(Duration.ofDays(1))
-//        	       .path("/")
-//        	       .build();
-        
+
         return ResponseEntity.ok()
-            .body(Map.of(
-                "accessToken", accessToken
-//                "refreshToken", refreshToken
-            ));
+            .body(tokenResponse);
     }
 
     @PostMapping("/verify")
-    public ResponseEntity<?> getVerificationCode(@RequestBody MembernameDTO membernameDTO) {
+    public ResponseEntity<?> getVerificationCode(@RequestBody MemberNameDTO membernameDTO) {
     	String username = membernameDTO.getUsername();
     	boolean check = verificationService.checkUser(username);
+
     	if (check) {
     		String code = verificationService.generateVerificationCode(username);
-    		return ResponseEntity.ok().body(Map.of("verificationCode", code));
-    	} else return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "incorrect username info"));
+            VerificationResponseDTO result = new VerificationResponseDTO(code);
+    		return ResponseEntity.ok()
+                    .body(result);
+    	}
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.builder()
+                        .message("Invalid user"));
     }
     
 }
