15b7b7a9d45c1953f29158160b3dbcc35d818433
leedongkyu0407 <use02012@gmail.com>
2025-01-23 03:18:31 +0900

    feat: 합성 파일 컨트롤러 추가

---
+++ src/main/java/com/ssafy/solvedpick/core/presentation/CompositionController.java
@@ -0,0 +1,30 @@
+package com.ssafy.solvedpick.core.presentation;
+
+import com.ssafy.solvedpick.core.service.CompositionService;
+import com.ssafy.solvedpick.members.domain.Member;
+import com.ssafy.solvedpick.members.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/compose")
+public class CompositionController {
+    private final CompositionService compositionService;
+    private final MemberRepository memberRepository;
+
+    @GetMapping(value = "/{username}", produces = "image/svg+xml")
+    public ResponseEntity<?> generateCompositeImage(@PathVariable String username) {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Member not found: "+username));
+
+        String svgImage = compositionService.generateCompositeImage(member);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("image/svg+xml"))
+                .body(svgImage);
+    }
+}
