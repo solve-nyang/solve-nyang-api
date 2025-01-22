c71b9a3709f01116ce2a47d59852577d87c3cbf5
leedongkyu0407 <use02012@gmail.com>
2025-01-22 21:22:52 +0900

    feat: 배경, 유저 보유 배경 엔티티 추가

---
+++ src/main/java/com/ssafy/solvedpick/ownedbackgrounds/domain/OwnedBackground.java
@@ -0,0 +1,41 @@
+package com.ssafy.solvedpick.ownedbackgrounds.domain;
+
+import com.ssafy.solvedpick.backgrounds.domain.Background;
+import com.ssafy.solvedpick.members.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "ownrd_backgrounds")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class OwnedBackground {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "background_id", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Background background;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean visible = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
