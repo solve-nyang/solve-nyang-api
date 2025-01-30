8c9979bf5112bc16766de23fd7351434dcfdc9d1
JIYOUNG KIM <kandallee38@gmail.com>
2025-01-31 02:54:09 +0900

    feat: Add feature
    Add feature search merchandise in auction in available
    TODO: return suitable response in result

---
+++ src/main/java/com/ssafy/solvedpick/auction/presentation/AuctionController.java
@@ -0,0 +1,29 @@
+package com.ssafy.solvedpick.auction.presentation;
+
+import com.ssafy.solvedpick.auction.facade.AuctionFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auction")
+public class AuctionController {
+
+    private final AuctionFacade auctionFacade;
+
+    // TODO: Use Converter
+    @GetMapping()
+    public ResponseEntity<?> searchMerchandise(
+            @RequestParam(value = "keyword", required = false) String keyword,
+            @RequestParam(value = "filter", defaultValue = "0") int filter,
+            @RequestParam(value = "page", defaultValue = "1") int page) {
+        // TODO: Get a response dto from facade
+        auctionFacade.searchMerchandise(keyword, filter, page);
+
+        return ResponseEntity.ok().build();
+    }
+}
