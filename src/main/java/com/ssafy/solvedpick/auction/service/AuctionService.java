1cd17879e581cf1cb5d268b3f7c057734750cbf3
JIYOUNG KIM <kandallee38@gmail.com>
2025-01-31 16:26:32 +0900

    feat: Add query
    Add query for using grade

--- src/main/java/com/ssafy/solvedpick/auction/service/AuctionService.java
+++ src/main/java/com/ssafy/solvedpick/auction/service/AuctionService.java
@@ -6,19 +6,35 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AuctionService {
 
     private final AuctionRepository auctionRepository;
 
+    @Transactional(readOnly = true)
     public Page<Auction> findMerchandiseWithKeyword(String keyword, Pageable pageable) {
         return auctionRepository.findAllByOwnedAvatar_Avatar_NameContainingAndSoldFalseAndCancelledFalse(
                 keyword, pageable);
     }
 
+    @Transactional(readOnly = true)
     public Page<Auction> findMerchandise(Pageable pageable) {
         return auctionRepository.findAllBySoldFalseAndCancelledFalse(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Auction> findMerchandiseWithKeywordAndGrade(String keyword, int grade, Pageable pageable) {
+        return auctionRepository
+                .findAllByOwnedAvatar_Avatar_NameContainingAndOwnedAvatar_Avatar_GradeAndSoldFalseAndCancelledFalse(
+                grade, keyword, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Auction> findMerchandiseWithGrade(int grade, Pageable pageable) {
+        return auctionRepository.findAllByOwnedAvatar_Avatar_GradeAndSoldFalseAndCancelledFalse(grade, pageable);
     }
 }
