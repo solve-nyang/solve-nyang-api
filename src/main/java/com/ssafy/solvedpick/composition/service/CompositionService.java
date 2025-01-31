a02ac56526ac403811adf94ced13e793aa640a86
leedongkyu0407 <use02012@gmail.com>
2025-02-01 01:23:09 +0900

    fix: dev서버 CD 테스트

--- src/main/java/com/ssafy/solvedpick/composition/service/CompositionService.java
+++ src/main/java/com/ssafy/solvedpick/composition/service/CompositionService.java
@@ -9,7 +9,6 @@ import com.ssafy.solvedpick.ownedbackgrounds.repository.OwnedBackgroundRepositor
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Slf4j
