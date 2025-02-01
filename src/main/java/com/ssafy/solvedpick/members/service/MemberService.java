61927f0e2c1ec035daa20275852e69fb303ade96
leedongkyu0407 <use02012@gmail.com>
2025-02-01 16:09:49 +0900

    fix: 유저 정보 업데이트 로직 수정
    - 유저 정보 업데이트 후 저장 로직 수정

--- src/main/java/com/ssafy/solvedpick/members/service/MemberService.java
+++ src/main/java/com/ssafy/solvedpick/members/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.ssafy.solvedpick.members.service;
 
 import com.ssafy.solvedpick.common.utils.point.Point;
+import com.ssafy.solvedpick.members.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.solvedpick.api.dto.SolvedProblemsApiResponse;
@@ -14,11 +16,13 @@ import com.ssafy.solvedpick.problem.domain.Problem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final ApiService apiService;
+    private final MemberRepository memberRepository;
 
     // TODO: 전체 수정하기
     public UserInfoResponse getUserInfo(Member member) {
@@ -37,10 +41,14 @@ public class MemberService {
         UserInfoApiResponse apiResponse = apiService.getUserInfo(member.getUsername());
         UserData userData = apiResponse.getItems().get(0);
 
+        log.debug("userData : {}", userData);
         SolvedProblemsApiResponse newProblems = apiService.getSolvedProblems(member.getUsername());
         Problem problem = member.getSolvedProblems();
 
         problem.updateSolvedProblems(newProblems);
         member.updateInfo(userData.getTier(), userData.getSolvedCount(), userData.getMaxStreak());
+
+        log.debug("member: {}", member);
+        memberRepository.save(member);
     }
 }
