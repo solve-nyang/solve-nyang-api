e5e8475059ac88ff4699a8a07990d868b7fffb60
leedongkyu0407 <use02012@gmail.com>
2025-02-03 23:52:20 +0900

    feat: Member에 OwnedBackground 필드 관계 설정

--- src/main/java/com/ssafy/solvedpick/members/domain/Member.java
+++ src/main/java/com/ssafy/solvedpick/members/domain/Member.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ssafy.solvedpick.ownedbackgrounds.domain.OwnedBackground;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -75,6 +76,10 @@ public class Member {
     @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OwnedAvatar> avatars = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OwnedBackground> backgrounds = new ArrayList<>();
 
     public void addPoint(long amount) {
         if (amount < 0) {
