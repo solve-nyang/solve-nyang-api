c2d37091f0ea34e39737da22f9430ef25486c88d
leedongkyu0407 <use02012@gmail.com>
2025-02-03 23:55:12 +0900

    feat: ownedBackground service 및 controller 생성

---
+++ src/main/java/com/ssafy/solvedpick/ownedbackgrounds/service/OwnedBackgroundService.java
@@ -0,0 +1,10 @@
+package com.ssafy.solvedpick.ownedbackgrounds.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OwnedBackgroundService {
+
+}
