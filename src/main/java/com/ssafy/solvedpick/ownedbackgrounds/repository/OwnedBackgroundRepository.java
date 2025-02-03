0d884a243ea061bca0a3929a0367b38b4a418385
leedongkyu0407 <use02012@gmail.com>
2025-02-03 23:53:03 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/com/ssafy/solvedpick/ownedbackgrounds/repository/OwnedBackgroundRepository.java
+++ src/main/java/com/ssafy/solvedpick/ownedbackgrounds/repository/OwnedBackgroundRepository.java
@@ -3,7 +3,6 @@ package com.ssafy.solvedpick.ownedbackgrounds.repository;
 import com.ssafy.solvedpick.members.domain.Member;
 import com.ssafy.solvedpick.ownedbackgrounds.domain.OwnedBackground;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
