8e3f18ac1d1609528a3c939f9bd54c249b905806
leedongkyu0407 <use02012@gmail.com>
2025-02-04 10:45:35 +0900

    feat: 배경화면 조회 API 엔드포인트 구현
    - 배경화면 목록 조회 컨트롤러 추가
    - 사용자별 배경화면 소유 여부 확인 로직 구현
    - BackgroundService, BackgroundRepository 구현

--- src/main/java/com/ssafy/solvedpick/backgrounds/repository/BackgroundRepository.java
+++ src/main/java/com/ssafy/solvedpick/backgrounds/repository/BackgroundRepository.java
@@ -1,10 +1,21 @@
 package com.ssafy.solvedpick.backgrounds.repository;
 
 import com.ssafy.solvedpick.backgrounds.domain.Background;
+import com.ssafy.solvedpick.backgrounds.dto.BackgroundQueryResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BackgroundRepository extends JpaRepository<Background, Long> {
 
-    Background findByName(String spaceField);
-
+    @Query("SELECT new com.ssafy.solvedpick.backgrounds.dto.BackgroundQueryResult(" +
+            "b.name, " +
+            "COUNT(ob) > 0 )" +
+            "FROM Background b " +
+            "LEFT JOIN OwnedBackground ob ON b = ob.background AND ob.member.id = :memberId " +
+            "GROUP BY b.name " +
+            "ORDER BY b.id")
+    List<BackgroundQueryResult> findAllWithOwnership(@Param("memberId") Long memberId);
 }
