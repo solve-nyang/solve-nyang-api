cc529a9c51ee8861ff29b5e85e0931d44cb24d28
leedongkyu0407 <use02012@gmail.com>
2025-02-04 13:51:01 +0900

    refactor: 전체 캐릭터 조회시 배경 id 함께 반환

--- src/main/java/com/ssafy/solvedpick/backgrounds/dto/BackgroundQueryResult.java
+++ src/main/java/com/ssafy/solvedpick/backgrounds/dto/BackgroundQueryResult.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 @Getter
 public class BackgroundQueryResult {
 
+    private Long id;
     private String name;
     private Boolean owned;
 
-    public BackgroundQueryResult(String name, Boolean owned) {
+    public BackgroundQueryResult(Long id, String name, Boolean owned) {
+        this.id = id;
         this.name = name;
         this.owned = owned;
     }
--- src/main/java/com/ssafy/solvedpick/backgrounds/service/BackgroundService.java
+++ src/main/java/com/ssafy/solvedpick/backgrounds/service/BackgroundService.java
@@ -1,13 +1,19 @@
 package com.ssafy.solvedpick.backgrounds.service;
 
+import com.ssafy.solvedpick.backgrounds.domain.Background;
 import com.ssafy.solvedpick.backgrounds.dto.BackgroundInfo;
 import com.ssafy.solvedpick.backgrounds.dto.BackgroundQueryResult;
 import com.ssafy.solvedpick.backgrounds.dto.BackgroundResponse;
 import com.ssafy.solvedpick.backgrounds.repository.BackgroundRepository;
 import com.ssafy.solvedpick.common.enums.BackgroundType;
+import com.ssafy.solvedpick.ownedbackgrounds.domain.OwnedBackground;
+import com.ssafy.solvedpick.members.domain.Member;
+import com.ssafy.solvedpick.ownedbackgrounds.repository.OwnedBackgroundRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +23,7 @@ import java.util.stream.Collectors;
 public class BackgroundService {
 
     private final BackgroundRepository backgroundRepository;
+    private final OwnedBackgroundRepository ownedBackgroundRepository;
 
     @Transactional(readOnly = true)
     public BackgroundResponse getAllBackgroundsWithOwnership(Long memberId) {
@@ -24,6 +31,7 @@ public class BackgroundService {
 
         List<BackgroundInfo> backgroundInfos = queryResults.stream()
                 .map(result -> BackgroundInfo.builder()
+                        .id(result.getId())
                         .name(result.getName())
                         .owned(result.getOwned())
                         .price(BackgroundType.fromName(result.getName()).getPrice())
@@ -32,4 +40,5 @@ public class BackgroundService {
 
         return BackgroundResponse.from(backgroundInfos);
     }
+
 }
