156d50eee78b107ebc4b3500afa9e4ae61a43809
JIYOUNG KIM <kandallee38@gmail.com>
2025-02-10 01:24:05 +0900

    fix: Change time

--- src/main/java/com/ssafy/solvedpick/problem/service/ProblemService.java
+++ src/main/java/com/ssafy/solvedpick/problem/service/ProblemService.java
@@ -7,8 +7,10 @@ import com.ssafy.solvedpick.members.domain.Member;
 import com.ssafy.solvedpick.problem.domain.Problem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ProblemService {
 
