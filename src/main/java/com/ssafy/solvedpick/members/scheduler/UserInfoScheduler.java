9a6f3ce4d1811e07f683810adf285a4202d0ecb2
leedongkyu0407 <use02012@gmail.com>
2025-02-09 23:11:49 +0900

    test: scheduling test

--- src/main/java/com/ssafy/solvedpick/members/scheduler/UserInfoScheduler.java
+++ src/main/java/com/ssafy/solvedpick/members/scheduler/UserInfoScheduler.java
@@ -17,7 +17,7 @@ public class UserInfoScheduler {
     private final MemberRepository memberRepository;
     private final MemberService memberService;
 
-    @Scheduled(cron = "0 0 4 * * *")
+    @Scheduled(cron = "0 15 23 * * *")
     public void updateUserInfo() {
         List<Member> members = memberRepository.findAll();
 
