6ee9fa4409e1c34825c4b2b46d96820d2a1db499
donggyu <use02012@gmail.com>
2025-02-12 17:56:49 +0900

    fix: tier 삽입 조건 추가

--- src/main/java/com/ssafy/solvedpick/composition/renderer/TierRenderer.java
+++ src/main/java/com/ssafy/solvedpick/composition/renderer/TierRenderer.java
@@ -12,6 +12,9 @@ public class TierRenderer {
     private SvgResources svgResources;
 
     public void renderTier(StringBuilder content, Tier tier) {
+        if(tier == null) {
+            return;
+        }
         content.append(String.format("<g transform=\"translate(%d, %d) scale(1)\">", 20, 20));
         content.append(tier.getSvgContent(svgResources));
         content.append("</g>");
